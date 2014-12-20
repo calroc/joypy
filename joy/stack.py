8edcad5112770a5bb0c7fc50e56aa6a1e6256ca7
Simon Forman <sforman@hushmail.com>
2014-12-20 13:13:35 -0800

    Fix up docs in stack module.

--- joy/stack.py
+++ joy/stack.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
+#
+#    Copyright © 2014, 2015 Simon Forman
+#
+#    This file is part of joy.py
+#
+#    joy.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    joy.py is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with joy.py.  If not see <http://www.gnu.org/licenses/>.
+#
 '''
 
 
 § Stack
 
+
 When talking about Joy we use the terms "stack", "list", "sequence" and
 "aggregate" to mean the same thing: a simple datatype that permits
-certain operations such as iterating through it and pushing and popping
-values from (at least) one end.
+certain operations such as iterating and pushing and popping values from
+(at least) one end.
 
 We use the venerable two-tuple recursive form of sequences where the
 empty tuple () is the empty stack and (head, rest) gives the recursive
@@ -20,6 +39,7 @@ form of a stack with one or more items on it.
   ...
 
 And so on.
+
 
 We have two very simple functions to build up a stack from a Python
 iterable and also to iterate through a stack and yield its items
@@ -35,6 +55,23 @@ of stacks:
   strstack()
 
 
+
+A word about the stack data structure.
+
+Python has very nice "tuple packing and unpacking" in its syntax which
+means we can directly "unpack" the expected arguments to a Joy function.
+
+For example:
+
+  def dup(stack):
+    head, tail = stack
+    return head, (head, tail)
+
+We replace the argument "stack" by the expected structure of the stack,
+in this case "(head, tail)", and Python takes care of de-structuring the
+incoming argument and assigning values to the names.  Note that Python
+syntax doesn't require parentheses around tuples used in expressions
+where they would be redundant.
 '''
 
 
@@ -70,44 +107,3 @@ def strstack(stack):
   if not stack: # shortcut
     return ''
   return ' '.join(map(stack_to_string, iter_stack(stack)))
-
-
-'''
-
-A word about the stack data structure.
-
-Python has very nice "tuple packing and unpacking" built into it in
-several places in its syntax, including (delightfully) function argument
-specifiers.  This means we can directly "unpack" the expected arguments
-to a Joy function right in the definition.
-
-For example:
-
-  def dup(stack):
-    head, tail = stack
-    return head, (head, tail)
-
-Becomes:
-
-  def dup((head, tail)):
-    return head, (head, tail)
-
-We replace the argument "stack" by the expected structure of the stack,
-in this case "(head, tail)", and Python takes care of de-structuring the
-incoming argument and assigning values to the names.  Note that Python
-syntax doesn't require parentheses around tuples used in expressions
-where they would be redundant but they are not redundant around the tuple
-appearing in the argspec.
-
-Consider:
-
-  def f((a, b)): ...
-
-  def g(a, b): ...
-
-Function f() expects one thing made of (a, b) while function g() expects
-two separate things.  The two argspecs are not equivalent.
-
-
-
-'''
