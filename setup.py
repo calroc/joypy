12894dea3d5ebd11528b4352121ae3be8a5f8c7a
Simon Forman <sforman@hushmail.com>
2015-04-11 15:17:54 -0700

    Initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+  name='Joypy',
+  version='0.1',
+  description='Python Implementation of Joy',
+  author='Simon Forman',
+  author_email='sforman@hushmail.com',
+  packages=['joy', 'joy.gui'],
+ )
