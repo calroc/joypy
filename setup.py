bb272ff0fef0ad19c1205ca83d6a9e4d62099769
Simon Forman <sforman@hushmail.com>
2015-04-12 17:18:37 -0700

    Bit o' Copyright.

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2014, 2015 Simon Forman
+#
+#    This file is part of joy.py
+#
+#    joy.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    joy.py is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with joy.py.  If not see <http://www.gnu.org/licenses/>.
+#
 from distutils.core import setup
 
 
